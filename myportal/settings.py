65774edd33d7208a204c99e7a97e7f299b81a92e
Iv05An <89o39752772@gmail.com>
2025-05-29 22:58:28 +0300

    fix setting.py

--- myportal/settings.py
+++ myportal/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('DJANGO_SECRET_KEY', default='your-default-secret-key')
 
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['django-tank-project.onrender.com', 'localhost', '127.0.0.1']
 
