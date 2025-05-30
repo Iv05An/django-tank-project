c0322643397f10ea839cb0ebf9878781322f7a0d
Iv05An <89o39752772@gmail.com>
2025-05-30 13:25:54 +0300

    Update ASGI, routing, and consumer for WebSocket

--- main/routing.py
+++ main/routing.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/article/(?P<slug>[^/]+)/$', consumers.ArticleConsumer.as_asgi()),
+    # re_path(r'ws/article/(?P<slug>[^/]+)/$', consumers.ArticleConsumer.as_asgi()),
+    re_path(r'ws/article/(?P<slug>[\w-]+)/$', consumers.ArticleConsumer.as_asgi()),
 ]
